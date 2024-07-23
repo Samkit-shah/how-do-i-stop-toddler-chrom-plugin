2036a110aab99aef47b82648850bd574a929b637
Samkit-shah <samkitbhai@gmail.com>
2024-07-23 17:44:04 -0400

    init

---
+++ content.js
@@ -0,0 +1,7 @@
+(function() {
+    const query = new URL(window.location.href).searchParams.get('q');
+    if (query && query.toLowerCase().startsWith('how do i stop a toddler from')) {
+      document.body.innerHTML = '<div style="display: flex; justify-content: center; align-items: center; height: 100vh;"><span style="font-size: 96px; font-family: Helvetica;">LMAO GOOD LUCK</span></div>';
+    }
+  })();
+  
